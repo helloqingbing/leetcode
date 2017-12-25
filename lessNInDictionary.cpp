02b1d2a9f75c8c80ed4a1657a2ac317cf36adbdc
Li <qbli@LimatoMacBook-Pro.local>
2017-12-25 16:58:13 +0800

    add comments

--- lessNInDictionary.cpp
+++ lessNInDictionary.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 using namespace std;
+/*
+题目具体输出举例：N=121
+
+0
+1
+10
+100
+101
+……
+108
+109
+11
+110
+111
+……
+
+118
+119
+12
+120
+121
+13
+……
+
+19
+2
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+3
+30
+……
+*/
 
 int dump(long long n) {
     long long total = 0;
@@ -23,7 +63,6 @@ int dump(long long n) {
     }
     return 0;
 }
-
 
 int main() {
     long long i = 150; 
